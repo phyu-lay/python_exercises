59575a516ed9b17756687befe01728f6539e5122
phyu-lay <phyuthwesoe19816@gmail.com>
2018-06-18 12:03:14 +0630

    Added ex42

---
+++ ex42.py
@@ -0,0 +1,11 @@
+class Animal(object):
+    pass
+class Dog(Animal):
+    def__init__(self, name):
+        self.name = name
+class Cat(Animal):
+            def__init__(self, name):
+                self.name = name
+class Person(object):
+    def__init__(self, name):
+        self.name = name
